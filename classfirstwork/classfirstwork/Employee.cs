ccafa94dc1812384f7fdf5fd084adad6715120e0
ferehibrahimli123@outlook.com <ferehibrahimli123@outlook.com>
2021-12-02 23:26:09 +0400

    firstclass

---
+++ classfirstwork/classfirstwork/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classfirstwork
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Positon;
+        public int DailySalary;
+
+        public Employee()
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Positon = Positon;
+            this.DailySalary = DailySalary;
+
+        }
+
+        public void Monthlysalary()
+        {
+            int DailySalary = 20;
+            int Monthlysalary = DailySalary * 30;
+            Console.WriteLine(DailySalary*30);
+        }
+    }
+}
---
+++ classfirstwork/classfirstwork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace classfirstwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+
+            employee1.Monthlysalary();
+            
+        }
+    }
+}
